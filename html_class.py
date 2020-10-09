95d5eda9cf5f75bfbd4907fb61dd181f367fbbfa
evelynchen1229 <evelynchen1229@gmail.com>
2020-10-09 22:43:55 +0100

    chainning methods with not very class programming way

--- html_class.py
+++ html_class.py
@@ -35,22 +35,22 @@ print(test.span())
 print(test.h1())
 # print(Format.div("foo")) error message: str doesn't have attribute get_argument
 
-'''part 2'''
-
-class div(Format):
-    def __init__(self, argument1,argument2=""):
-        Format.__init__(self, argument1, argument2="")
-        #super().__init__(self,argument)
-
-    def p(self):
-        text = self.get_argument()
-        return f"<div><p>{text}</p><div>"
-  #  def p.span(self):
-  #      text = self.get_argument()
-  #      return "<div><p><span>text</span></p></div>"
-
-
-
-test = div("foo")
-print(test.p())
-#print(test.p.span())
+#'''part 2'''
+#
+#class div(Format):
+#    def __init__(self, argument1,argument2=""):
+#        Format.__init__(self, argument1, argument2="")
+#        #super().__init__(self,argument)
+#
+#    def p(self):
+#        text = self.get_argument()
+#        return f"<div><p>{text}</p><div>"
+#  #  def p.span(self):
+#  #      text = self.get_argument()
+#  #      return "<div><p><span>text</span></p></div>"
+#
+#
+#
+#test = div("foo")
+#print(test.p())
+##print(test.p.span())
