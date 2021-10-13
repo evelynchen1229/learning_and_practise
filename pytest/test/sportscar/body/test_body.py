00b687d81022e9ccbbfaa5a802f4d60b1e2d6d41
evelynchen1229 <eveynchen1229@gmail.com>
2021-10-13 23:18:45 +0100

    test web browser navigation

--- pytest/test/sportscar/body/test_body.py
+++ pytest/test/sportscar/body/test_body.py
@@ -1,6 +1,10 @@
+from selenium import webdriver
 from pytest import mark
 
 @mark.smoke
+@mark.ui
 
-def test_body_functions_as_expected():
+def test_can_navigate():
+    browser = webdriver.Chrome()
+    browser.get('https://www.google.co.uk')
     assert True
