652006028b483da4feb5fd9507d3ad47f8b25212
Evelyn Chen <evelynchen1229@gmail.com>
2021-10-15 21:28:29 +0100

    test fixture

--- pytest/test/sportscar/body/test_body.py
+++ pytest/test/sportscar/body/test_body.py
@@ -1,10 +1,8 @@
-from selenium import webdriver
 from pytest import mark
 
 @mark.smoke
 @mark.ui
 
-def test_can_navigate():
-    browser = webdriver.Chrome()
-    browser.get('https://www.google.co.uk')
+def test_can_navigate(chrome_browser):
+    chrome_browser.get('https://www.google.co.uk')
     assert True
--- pytest/test/sportscar/engine/test_engine.py
+++ pytest/test/sportscar/engine/test_engine.py
@@ -2,6 +2,12 @@ from pytest import mark
 
 @mark.engine
 @mark.smoke
+@mark.ui
 
 def test_engine_functions_as_expected():
     assert True
+
+def test_can_navigate_to_youtube(chrome_browser):
+    chrome_browswer.get('https://www.youtube.com')
+    assert True
+
