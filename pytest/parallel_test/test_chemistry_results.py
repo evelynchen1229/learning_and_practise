b89f6ae4eb10f3a1cbff464c3e40bbda0ee81373
Evelyn Chen <evelynchen1229@gmail.com>
2021-10-21 23:43:41 +0100

    parallel_test

---
+++ pytest/parallel_test/test_chemistry_results.py
@@ -0,0 +1,34 @@
+''' Need to have tests isolated to use parallel testing strategy!!! '''
+import time
+
+def test_result_1_completes_expected():
+    time.sleep(5)
+    print("Result 1 has completed")
+
+def test_result_2_completes_expected():
+    time.sleep(5)
+    print("Result 2 has completed")
+
+def test_result_3_completes_expected():
+    time.sleep(5)
+    print("Result 3 has completed")
+
+def test_result_4_completes_expected():
+    time.sleep(5)
+    print("Result 4 has completed")
+
+def test_result_5_completes_expected():
+    time.sleep(5)
+    print("Result 5 has completed")
+
+def test_result_6_completes_expected():
+    time.sleep(5)
+    print("Result 6 has completed")
+
+def test_result_7_completes_expected():
+    time.sleep(5)
+    print("Result 7 has completed")
+
+def test_result_8_completes_expected():
+    time.sleep(5)
+    print("Result 8 has completed")
