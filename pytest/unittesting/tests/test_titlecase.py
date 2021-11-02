f016af1abe08136b99e74292ae3cc47f912e4feb
evelynchen1229 <evelynchen1229@gmail.com>
2021-11-02 21:28:02 +0000

    test titlecase

--- pytest/unittesting/tests/test_titlecase.py
+++ pytest/unittesting/tests/test_titlecase.py
@@ -1,5 +1,7 @@
 from titlecase import title_case
 
-class TitleCaseTests:
-    def test_text_is_uppercased_as_expected(self):
-
+def test_title_capitalised_as_expected():
+    initial = 'this is a test'
+    expected = 'This Is a Test'
+    actual = title_case(initial)
+    assert expected == actual
