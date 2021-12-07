a2659fc658e9e80d5feb53cf0dc280b9b67b2373
evelynchen1229 <evelynchen1229@gmail.com>
2021-12-07 00:42:21 +0000

    day 6 part2

--- adventofcode/2021/day6/main.py
+++ adventofcode/2021/day6/main.py
@@ -1,88 +1,39 @@
-import math
-
 from construction import constructor
-initial_state = constructor('test_input.txt')
-#initial_state = constructor('input.txt')
-#print(initial_state)
+#initial_state = constructor('test_input.txt')
+initial_state = constructor('input.txt')
 
-# part 1
 def number_of_fish(state,days):
-    num_of_fish = len(state)
-    if days > 0:
-        days -= 1
-        for fish_number in range(0,num_of_fish):
-            fish = state[fish_number]
-            if fish == 0:
-                state[fish_number] = 6
-                state.append(8)
-            else:
-                state[fish_number] -= 1
-
-        return number_of_fish(state,days)
-
-    return state
-
-test = number_of_fish([3],18)
-#print(test)
-total_number_of_fish = len(test)
-print(total_number_of_fish)
+    previous_state = {'0':state.count(0),
+                '1': state.count(1),
+                '2': state.count(2),
+                '3': state.count(3),
+                '4': state.count(4),
+                '5': state.count(5),
+                '6': state.count(6),
+                '7': state.count(7),
+                '8': state.count(8)
+                }
 
 
-def synced_fish(state):
-    synced = []
-    for i in range(0,7):
-        synced.append(state.count(i))
-    return synced
+    for day in range(0,days):
+        initial_0 = previous_state['0']
+        previous_state['0'] = previous_state['1']
+        previous_state['1'] = previous_state['2']
+        previous_state['2'] = previous_state['3']
+        previous_state['3'] = previous_state['4']
+        previous_state['4'] = previous_state['5']
+        previous_state['5'] = previous_state['6']
+        previous_state['6'] = initial_0 + previous_state['7']
+        previous_state['7'] = previous_state['8']
+        previous_state['8'] = initial_0
 
-test = synced_fish(initial_state) 
-print(test)
+    total_fish = sum(previous_state.values())
+    return total_fish 
+# part 1
+fish_volume = number_of_fish(initial_state,80)
+print(fish_volume)
 
+# part 2
 
-def num_of_fish_created(fish,days):
-    if fish >= days:
-        return 0
-    else:
-        cycles = math.ceil((days - fish) / 7)
-    return cycles
-
-def num_of_fish_can_reproduce(fish,days):
-    if fish >= days:
-        return 0
-    else:
-        full_cycles = math.floor((days - fish) / 7)
-    return full_cycles
-
-def total_fish(fish,days):
-    # get total number of fish produced by the first fish
-    num_fish_created = num_of_fish_created(fish,days)
-    if num_fish_created == 0:
-        return 1 # initial fish
-    else:
-        for num_fish in range(0,num_fish_created):
-            #days_before_cycle
-            fish += num_fish * 7 + 2 + 7
-            #find how many fish being created by each fish created by the first fish
-            fish_created = num_of_fish_created(fish, days)
-            if fish_created == 0:
-                num_fish_created += 1
-                return num_fish_created
-            else:
-                num_fish_created += fish_created
-                # find how many of the fish created can actually have time to reproduce
-                fish_can_reproduce = num_of_fish_can_reproduce(fish,days)
-                if fish_can_reproduce == 0:
-                    total_fish = 1 + num_fish_created # add the initial fish
-                    return total_fish
-                else:
-                # repeat the procedure
-                       pass
-
-test = total_fish(1,15)
-print(test)
-
-
-
-            
-
-
-
+huge_number_of_fish = number_of_fish(initial_state,256)
+print(huge_number_of_fish)
