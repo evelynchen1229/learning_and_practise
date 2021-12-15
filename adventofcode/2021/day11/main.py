4ed1caa702e468d402f7d251aa4f22224ffe04bb
evelynchen1229 <evelynchen1229@gmail.com>
2021-12-15 12:35:16 +0000

    day 10

--- adventofcode/2021/day11/main.py
+++ adventofcode/2021/day11/main.py
@@ -3,13 +3,12 @@ import numpy as np
 
 test_lines = constructor('test_input.txt')
 sample_lines = constructor('sample.txt')
+challenge_lines = constructor('input.txt')
 
-cols = test_lines['max_x']
-rows = test_lines['max_y']
+cols = challenge_lines['max_x']
+rows = challenge_lines['max_y']
 
 def adjacent(position):
-    print(position)
-    #print(position)
     x = position[0]
     y = position[1]
 
@@ -27,7 +26,6 @@ def adjacent(position):
     legal_adjacents = [p for p in adjacent_positions if 0 <= p[0] < cols and 0 <= p[1] < rows]
 
     return legal_adjacents
-
 def energy_number(position,level_map):
     row = position[1]
     col = position[0]
@@ -38,67 +36,51 @@ def flash_level_list( energy_level_map):
     for row in range(0, rows):
         for col in range(0, cols):
             if energy_level_map[row][col] == 10:
-                #print((col,row))
                 flashy.append((col, row))
-    print("flasy:",flashy)
     return flashy
 
 
 def adjacent_level_increase(flash_position, energy_level_map):
-    for flash_point in flash_position:
-        print(flash_point)
-        adjacent_positions = adjacent(flash_point)
+    adjacent_positions = adjacent(flash_position)
 
-        new_flash = []
-   #       print("adjacents:",adjacent_positions)
-        for adjacent_position in adjacent_positions:
-            adjacent_row = adjacent_position[1]
-            adjacent_col = adjacent_position[0]
-   #         print("adjacents point:", adjacent_position)
-   #         print("adjacent energy:", energy_number(adjacent_position, energy_level_map))
+    new_flashes = []
+    for adjacent_position in adjacent_positions:
+        adjacent_row = adjacent_position[1]
+        adjacent_col = adjacent_position[0]
 
-            if  energy_number(adjacent_position, energy_level_map) < 10:
-                energy_level_map[adjacent_row][adjacent_col] += 1
-                adjacent_energy = energy_level_map[adjacent_row][adjacent_col]#energy_number(adjacent_position, energy_level_map)
-                if adjacent_energy == 10:
-                    print(adjacent_position)
-                    new_flash.append(adjacent_position)
-                    print(new_flash)
-                else:
-                    pass
-            else:
-                pass
-        print(new_flash)
-        if len(new_flash) == 0:
-            print("yes")
-            return energy_level_map
-        else:
-            return adjacent_level_increase(new_flash, energy_level_map)
-   # return energy_level_map
+        if  energy_number(adjacent_position, energy_level_map) < 10:
+            energy_level_map[adjacent_row][adjacent_col] += 1
+            adjacent_energy = energy_number(adjacent_position, energy_level_map)
+
+            if adjacent_energy == 10:
+                new_flashes.append(adjacent_position)
+
+    if len(new_flashes) == 0:
+        return energy_level_map
+    elif len(new_flashes) == 1:
+        return adjacent_level_increase(new_flashes[0], energy_level_map)
+    else:
+        for new_flash in new_flashes:
+            enery_level_map = adjacent_level_increase(new_flash, energy_level_map)
+
+    return energy_level_map
 
 
 def flash( energy_level_map , position ):
     'level increase by 1 in the beginning of the step'
     energy_level_map += 1
-    print("energy map",energy_level_map)
 
     'find all the original flash level'
     flash_points = flash_level_list(energy_level_map)
-    print("flash points",flash_points)
-    #print(flash_points)
+
     '''find all the adjacent points for the flash points
     adjacent level increase by 1
     if any adjacent level reach flash points - repeat process until no adjacent level is flashy
     '''
     for p in flash_points:
-#        print(p)
-        energy_level_map = adjacent_level_increase([p],energy_level_map)
+        energy_level_map = adjacent_level_increase(p,energy_level_map)
 
     energy_level_map = np.where(energy_level_map == 10, 0, energy_level_map)
-
-#    after_n_steps = {'new_energy': energy_level_map,
-#            'num_of_flashes': flashes
- #           }
 
     return energy_level_map
 
@@ -106,15 +88,26 @@ def num_flash(step, energy_level_map = sample_lines['energy_level'],  position =
     flashes = 0
     for s in range(0, step):
         energy_level_map = flash(energy_level_map, position)
-        print(energy_level_map)
         flashes += np.count_nonzero(energy_level_map == 0)
 
     return flashes
 
-#test = flash()#, test_lines, test_lines['energy_level'], test_lines['position'])
-test_flashes = num_flash(2, test_lines['energy_level'],test_lines['position'])
-print(test_flashes)
-#print(test)
+
+def syncronise(step = 0,energy_level_map = test_lines['energy_level'],position = test_lines['position']):
+    num_flashes = 0
+    while num_flashes < 100:
+         energy_level_map = flash(energy_level_map,position)
+         num_flashes = np.count_nonzero(energy_level_map == 0)
+         step += 1
+    return step
+
+'''part 1'''
+challange_flashes = num_flash(100, challenge_lines['energy_level'],challenge_lines['position'])
+print("part 1:",challange_flashes)
+
+'''part 2'''
+challenge_all_flash = syncronise(energy_level_map = challenge_lines['energy_level'],position=challenge_lines['position'])
+print("part2:", challenge_all_flash)
 
 
 
--- adventofcode/2021/day9/part2.py
+++ adventofcode/2021/day9/part2.py
@@ -8,7 +8,6 @@ challenge_heightmaps = constructor('input.txt')
 test_low_points = low_points(test_heightmaps)['low_point_positions']
 challenge_low_points = low_points(challenge_heightmaps)['low_point_positions']
 
-#print(test_low_points)
 
 maxium_x = len(challenge_heightmaps[0])
 maxium_y = len(challenge_heightmaps)
@@ -46,27 +45,17 @@ def basin(center_point, heightmap, basin_list = [], depth = 0):
     center_point_height = heightmap[center_point_row][center_point_col]
 
     points_around_point = adjacent_points(center_point)
-    additional_points = [point for point in points_around_point if heightmap[point[1]][point[0]] < 9 and heightmap[point[1]][point[0]] >= center_point_height and point not in basin_list]
+    additional_points = [point for point in points_around_point if heightmap[point[1]][point[0]] < 9 and point not in basin_list and heightmap[point[1]][point[0]] >= center_point_height and point not in basin_list]
 
-    #additional_points = filter points_around_point: height < 9 and height >= low_point_height and height not in basin_list
-
-#    print("depth=", depth)
-#    print("basin list: ", basin_list)
-#    print("additional points", additional_points)
     points_with_additional = basin_list + additional_points
-#    print("points with additional: ", points_with_additional)
 
     return dedup([center_point] + flatten([basin(point, heightmap, points_with_additional, depth + 1) for point in additional_points]))
-    #return points_with_additional + (map additional_points basin(point, heightmap, points_with_additional))
     
-#test = basin((9,0),test_heightmaps)
-#print(test)
 basins_size = []
-#print(len(challenge_low_points))
 for low_point in challenge_low_points:
     basin_result = basin(low_point,challenge_heightmaps)
     basins_size.append(len(basin_result))
-#print(basins_size)
 basins_size.sort(reverse=True)
+
 part2 = basins_size[0] * basins_size[1] * basins_size[2]
 print(part2)
