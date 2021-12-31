b6001451484a7bf8b08f9f4c5300e955a2d7b3d3
evelynchen1229 <evelynchen1229@gmail.com>
2021-12-31 11:38:55 +0000

    day15

--- adventofcode/2021/day15/main.py
+++ adventofcode/2021/day15/main.py
@@ -1,149 +1,73 @@
 from construction import constructor
-from warnings import warn
 from bigmap import fullmaze 
+import math
+import numpy as np
 
+''' part 1'''
 #challenge = constructor('input.txt')
+
+''' part 2'''
 challenge = fullmaze('input.txt')
-maze = challenge['position']
+
+maze = maze_map(challenge['position'])
 cost = challenge['values']
-maze_length = len(cost[0])
-maze_height = len(cost)
-start = min(maze)
-end = max(maze)
+maze_shape = np.shape(cost)
+max_rows = maze_shape[0]
+max_cols = maze_shape[1]
 
-diagonal = False
+initial_t_dist = np.zeros((maze_shape))+math.inf
+initial_t_dist[0,0] = 0
 
-class Node():
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
+def maze_map(position_list):
+    maze_position = [position[::-1] for position in position_list]
+    return maze_position
 
-        self.g = cost[self.position[1]][self.position[0]] 
-        self.h = 0#  (end[0] - self.position[0])   + (end[1] - self.position[1]) 
-        self.f = self.g + self.h 
+def adjacents(current_node, max_rows, max_cols):
+    row = current_node[0]
+    col = current_node[1]
 
-    def __eq__(self, other):
-        return self.position == other.position
-
-    def __repr__(self):
-        return f"{self.position}"
+    above = [row - 1, col]
+    below = [row + 1, col]
+    left = [row, col - 1]
+    right = [row, col + 1]
     
-    def __lt__(self, other):
-        return self.f < other.f
+    adjacent_nodes = [above, below, left, right]
 
-    def __gt__(self, other):
-        return self.f > other.f
-
-
-def adjacents(current_node):
-    x = current_node.position[0]
-    y = current_node.position[1]
-
-    above = [x, y-1]
-    below = [x, y+1]
-    left = [x-1, y]
-    right = [x+1, y]
-    diagonal_upperleft = [x-1, y-1]
-    diagonal_upperright = [x+1, y-1]
-    diagonal_lowerleft = [x-1, y+1]
-    diagonal_lowerright = [x+1, y+1]
-
-    if diagonal:
-        adjacent_nodes = [above, below, left, right, diagonal_upperleft, diagonal_upperright, diagonal_lowerleft, diagonal_lowerright]
-    else:
-        adjacent_nodes = [above, below, left, right]
-
-    legal_adjacents = [adjacent for adjacent in adjacent_nodes if 0 <= adjacent[0] < maze_length and 0 <= adjacent[1] < maze_height]
+    legal_adjacents = [adjacent for adjacent in adjacent_nodes if 0 <= adjacent[0] < max_rows and 0 <= adjacent[1] < max_cols]
 
     return legal_adjacents
 
-def return_path(current_node):
-    path = []
-    node_cost = 0
-    current = current_node
-    while current is not None:
-        path.append(current.position)
-        node_cost += current.g
-        current = current.parent
 
-    least_cost = {'path': path[::-1],
-            'cost': node_cost}
-    return least_cost 
+def dijkstra():
+    current_node = [0,0]
+    unvisited_nodes = maze
+    t_dist = initial_t_dist
 
-def current_cost(current_node):
-    node_cost = 0
-    current = current_node
-    while current is not None:
-        node_cost += current.f
-        current = current.parent
-    return node_cost
-    
-def prioritise(open_list, current_node):
-    list_size = len(open_list)
-    if list_size == 0:
-        open_list.append(current_node)
-    elif current_cost(current_node) > max([current_cost(node) for node in open_list]):
-        open_list.append(current_node)
-    else:
+    destination = max(maze)
 
-        for n in range(0, list_size):
-            existing_node = open_list[n]
-            if current_cost(existing_node) == current_cost(current_node):
-                open_list = open_list[:n + 1] + [current_node] + open_list[n + 1:]
-                break
-            elif current_cost(existing_node) > current_cost(current_node):
-                if existing_node.position == current_node.position:
-                    open_list[n] = current_node
-                else:
-                    open_list = open_list[:n] + [current_node] + open_list[n:]
-                break
-    
-    return open_list
+    while True:
+        print(current_node)
 
+        if len(unvisited_nodes) == 0:
+            raise Exception("Did not get to destination")
 
+        if current_node == destination:
+            total_cost = t_dist[current_node[0],current_node[1]]
+            return total_cost
 
-def astar():
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
+        unvisited_nodes.remove(current_node)
 
-    end_node = Node(None, end)
+        neighbour_nodes = adjacents(current_node, max_rows, max_cols)
+        unvisited_neighbours = [node for node in neighbour_nodes if node in unvisited_nodes]
 
-    open_list = []
-    closed_list = []
+        for node in unvisited_neighbours: 
+            tentative_dist = t_dist[current_node[0],current_node[1]] + cost[node[0]][node[1]] 
+            if tentative_dist < t_dist[node[0],node[1]]:
+                t_dist[node[0],node[1]] = tentative_dist
+        
+        unvisited_cost = [t_dist[node[0],node[1]] for node in unvisited_nodes]
+        index = unvisited_cost.index(min(unvisited_cost))
+        current_node = unvisited_nodes[index]
 
-    open_list.append(start_node)
-
-    i = 0
-
-    while len(open_list ) > 0 :
-        i += 1
-        current_node = open_list[0]
-        if i % 1000 == 0:
-            print(current_node)
-        open_list.pop(0)
-        closed_list.append(current_node)
-    
-        if current_node == end_node:
-            return return_path(current_node)
-        else:
-            next_nodes = adjacents(current_node)
-            for node in next_nodes:
-                new_node = Node(current_node, node)
-                if len([open_node for open_node in open_list if new_node.position == open_node.position and current_cost(new_node) >= current_cost(open_node)]) > 0:
-                    pass
-                elif len([closed_node for closed_node in closed_list if new_node.position == closed_node.position and current_cost(new_node) >= current_cost(closed_node)]) > 0:
-                    pass
-                else:
-                    open_list = prioritise(open_list,new_node)
-                
-
-    warn("no path found")
-    return None
-    
-def main():
-    path = astar()
-    print(path)
-
-if __name__ == '__main__':
-    main()
-
+test = dijkstra()
+print(test)
